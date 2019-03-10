8519bda96d631f28c5a49990401ac5907e244a1d
Ryan Davis <ryand@zenspider.com>
2019-03-10 02:31:59 -0800

    oops. needed to tag for 2.6 in pt_testcase.rb [git-p4: depot-paths = "//src/sexp_processor/dev/": change = 11826]

--- lib/pt_testcase.rb
+++ lib/pt_testcase.rb
@@ -77,7 +77,7 @@ class ParseTreeTestCase < Minitest::Test
   end
 
   def self.add_19tests name, hash
-    add_tests "#{name}__19_20_21_22_23_24_25", hash # HACK?
+    add_tests "#{name}__19_20_21_22_23_24_25_26", hash # HACK?
   end
 
   def self.add_19edgecases ruby, sexp, cases
@@ -102,7 +102,7 @@ class ParseTreeTestCase < Minitest::Test
     testcases[verbose][klass] = testcases[nonverbose][klass]
   end
 
-  VER_RE = "(1[89]|2[012345])"
+  VER_RE = "(1[89]|2[0123456])"
 
   def self.generate_test klass, node, data, input_name, output_name
     klass.send :define_method, "test_#{node}" do
